6a1ef86a2d5b253f9a7a31ecd91075a2cf70119f
Mohan-Sanjay <166888236+Mohan-Sanjay@users.noreply.github.com>
2025-02-15 18:38:29 +0530

    Time: 0 ms (100%), Space: 23.4 MB (16.29%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int>left(ratings.size(),1),right(ratings.size(),1);
+        for(int i=1;i<ratings.size();i++)
+            if(ratings[i]>ratings[i-1])
+                left[i]=left[i-1]+1;
+        for(int i=ratings.size()-2;i>=0;i--)
+            if(ratings[i]>ratings[i+1])
+            right[i]=right[i+1]+1;
+        int m=0;
+        for(int i=0;i<ratings.size();i++)
+            m+=max(left[i],right[i]);
+        return m;
+    }
+};
